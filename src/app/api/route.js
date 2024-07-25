4a3a0572dd3552268c938738f79282f2044e3e84
kibxz <kibzy445@gmail.com>
2024-07-25 15:25:18 +0700

    Update route.js

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -17,7 +17,7 @@ export async function GET() {
       });
 
     } catch (error) {
-      return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      return new Response(JSON.stringify( error:), {
         status: 500,
         headers: { "Content-Type": "application/json" },
       });
